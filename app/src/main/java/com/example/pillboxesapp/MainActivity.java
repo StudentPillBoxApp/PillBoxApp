b56c048c1ea972b6bf23a4dec2f7bc13f4e9974f
JLP98 <jlp98@protonmail.com>
2019-11-21 13:45:09 +0000

    Added Project to Git

---
+++ app/src/main/java/com/example/pillboxesapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.pillboxesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnRegister = (Button) findViewById(R.id.btnSignUp);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterActivity();
+            }
+        });
+    }
+
+    public void openRegisterActivity() {
+        Intent intent = new Intent(this, PatientSignUpScreen.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/pillboxesapp/PatientSignUpScreen.java
@@ -0,0 +1,14 @@
+package com.example.pillboxesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientSignUpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_sign_up_screen);
+    }
+}
